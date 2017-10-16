7455cccb3ed8d55ef6e9263984d7b14a9b5c335b
Tim Ninan <tim.ninan@gmail.com>
2017-10-16 15:34:34 -0500

    completed siskel tutorial

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,7 +6,11 @@ var Movie = Backbone.Model.extend({
 
   toggleLike: function() {
     // your code here
+    this.set('like', !this.get('like'));
+    //this.collection.sort();
+
   }
+
 
 });
 
@@ -16,12 +20,18 @@ var Movies = Backbone.Collection.extend({
 
   initialize: function() {
     // your code here
+    this.on('change:like', function() {
+      this.sort();
+    }, this);
+
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
     // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -59,6 +69,11 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.model.on('change', function() {
+      this.render();
+    }, this);
+    
+
   },
 
   events: {
@@ -67,6 +82,7 @@ var MovieView = Backbone.View.extend({
 
   handleClick: function() {
     // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -80,6 +96,11 @@ var MoviesView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.collection.on('sort', function() {
+      this.render();
+    }, this);
+    //console.log('THIS IS EQUAL TO', this);
+
   },
 
   render: function() {
